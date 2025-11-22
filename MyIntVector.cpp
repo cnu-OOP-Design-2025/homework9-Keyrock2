9c60d624a021dd437c93ac9141e0122a7a7b305d
Keyrock2 <mbrock1047@gmail.com>
2025-11-22 21:03:53 +0900

    "과제제출"

--- MyIntVector.cpp
+++ MyIntVector.cpp
@@ -2,64 +2,88 @@
 #include <list>
 using namespace std;
 
-class MyIntVector {
+class MyIntVector
+{
 private:
-    int* data;           // 동적 배열을 가리키는 포인터
-    size_t capacity;     // 현재 할당된 배열 크기
-    size_t length;       // 실제 저장된 요소 개수
+    int *data;       // 동적 배열을 가리키는 포인터
+    size_t capacity; // 현재 할당된 배열 크기
+    size_t length;   // 실제 저장된 요소 개수
 
-    void resize() {
+    void resize()
+    {
         cout << "Resize: " << capacity << " -> " << capacity * 2 << endl;
 
-        /* TODO */
-        // capacity의 크기를 2배로 늘리고, 새로운 배열을 생성하세요.
-        // 기존 데이터를 새로운 배열로 복사한 뒤, 기존 배열을 해제하세요.
+        /* TODO: capacity의 크기를 2배로 늘리고, 새로운 배열을 생성하세요. */
+        size_t newCapacity = capacity * 2;
+        int *newData = new int[newCapacity];
+
+        /* TODO: 기존 데이터를 새로운 배열로 복사한 뒤, 기존 배열을 해제하세요. */
+        for (size_t i = 0; i < length; ++i)
+        {
+            newData[i] = data[i];
+        }
+
+        delete[] data;          // 기존 메모리 해제
+        data = newData;         // 포인터 갱신
+        capacity = newCapacity; // capacity 값 갱신
     }
 
 public:
-
     // 초기 capacity를 2로 설정하고, length는 0으로 초기화하세요.
-    MyIntVector() : capacity(2), length(0) {
-        /* TODO */
-        // data는 capacity 크기의 배열을 동적 할당하세요.
+    MyIntVector() : capacity(2), length(0)
+    {
+        /* TODO: data는 capacity 크기의 배열을 동적 할당하세요. */
+        data = new int[capacity];
     }
 
-    ~MyIntVector() {
-        /* TODO */
-        // 동적 할당된 data를 해제하세요.
+    ~MyIntVector()
+    {
+        /* TODO: 동적 할당된 data를 해제하세요. */
+        if (data != nullptr)
+        {
+            delete[] data;
+        }
     }
 
-    void push_back(const int& value) {
-        /* TODO */
-        // length가 capacity에 도달하면 resize()를 호출하세요.
+    void push_back(const int &value)
+    {
+        /* TODO: length가 capacity에 도달하면 resize()를 호출하세요. */
+        if (length == capacity)
+        {
+            resize();
+        }
 
-        /* TODO */
-        // 새로운 요소를 배열 끝에 추가하고 length를 증가시키세요.
+        /* TODO: 새로운 요소를 배열 끝에 추가하고 length를 증가시키세요. */
+        data[length] = value;
+        length++;
     }
 
     // 마지막 항목을 리턴하세요.
-    int pop_back() {
+    int pop_back()
+    {
         if (length == 0)
             throw out_of_range("Vector is empty");
-        return data[--length]; // 마지막 요소 제거
+        return data[--length]; // 마지막 요소 제거 (실제 메모리는 유지, 인덱스만 줄임)
     }
 
     // 주어진 index의 요소를 반환하세요.
-    int& operator[](const int index){
+    int &operator[](const int index)
+    {
         return data[index];
     }
 
     // 현재 저장된 요소 개수를 반환하세요.
-    size_t size() const {
+    size_t size() const
+    {
         return length;
     }
-
 };
-void testVector(const string& name, std::list<int> values) {
 
+void testVector(const string &name, std::list<int> values)
+{
     cout << "------------------" << endl;
     MyIntVector vec;
-    for (auto& v : values)
+    for (auto &v : values)
         vec.push_back(v);
 
     cout << name << ": ";
@@ -67,11 +91,12 @@ void testVector(const string& name, std::list<int> values) {
         cout << vec[i] << " ";
 
     cout << endl;
-    while(vec.size())
+    while (vec.size())
         cout << "Pop: " << vec.pop_back() << "\n";
 }
 
-int main() {
+int main()
+{
     testVector("IntVec", {1, 2, 3, 4, 5, 6});
     return 0;
 }
--- MyVector.cpp
+++ MyVector.cpp
@@ -3,35 +3,100 @@
 #include <list>
 using namespace std;
 
-template<typename T>
-class MyVector {
-/* TODO */
+template <typename T>
+class MyVector
+{
+private:
+    T *data; // 데이터 타입이 int에서 T로 변경됨
+    size_t capacity;
+    size_t length;
 
+    void resize()
+    {
+        cout << "Resize: " << capacity << " -> " << capacity * 2 << endl;
+        size_t newCapacity = capacity * 2;
+        T *newData = new T[newCapacity]; // int 대신 T 타입 배열 할당
 
+        for (size_t i = 0; i < length; ++i)
+        {
+            newData[i] = data[i];
+        }
 
+        delete[] data;
+        data = newData;
+        capacity = newCapacity;
+    }
 
+public:
+    MyVector() : capacity(2), length(0)
+    {
+        data = new T[capacity]; // int 대신 T 타입 할당
+    }
 
+    ~MyVector()
+    {
+        if (data != nullptr)
+        {
+            delete[] data;
+        }
+    }
 
+    void push_back(const T &value)
+    { // 매개변수 타입이 T로 변경됨
+        if (length == capacity)
+        {
+            resize();
+        }
+        data[length++] = value;
+    }
+
+    T pop_back()
+    { // 반환 타입이 T로 변경됨
+        if (length == 0)
+            throw out_of_range("Vector is empty");
+        return data[--length];
+    }
+
+    T &operator[](const int index)
+    { // 반환 타입이 T&로 변경됨
+        return data[index];
+    }
+
+    size_t size() const
+    {
+        return length;
+    }
+
+    // 심화 학습: 범위 기반 for 문을 위한 begin, end 구현
+    T *begin() { return data; }
+    T *end() { return data + length; }
 };
 
-template<typename T>
-void testVector(const string& name, std::list<T> values) {
+template <typename T>
+void testVector(const string &name, std::list<T> values)
+{
 
     cout << "------------------" << endl;
     MyVector<T> vec;
-    for (auto& v : values)
+    for (auto &v : values)
         vec.push_back(v);
 
     cout << name << ": ";
-    for (size_t i = 0; i < vec.size(); ++i)
-        cout << vec[i] << " ";
+
+    // 심화 학습: 범위 기반 for문 적용
+    // 원래 코드: for (size_t i = 0; i < vec.size(); ++i) cout << vec[i] << " ";
+    for (auto &e : vec)
+    {
+        cout << e << " ";
+    }
 
     cout << endl;
-    while(vec.size())
+    while (vec.size())
         cout << "Pop: " << vec.pop_back() << "\n";
 }
 
-int main() {
+int main()
+{
     testVector<int>("IntVec", {1, 2, 3, 4, 5, 6});
     testVector<double>("DoubleVec", {1.1, 2.2, 3.3, 4.4, 5.5});
     testVector<char>("CharVec", {'A', 'B', 'C', 'D', 'E', 'F'});
